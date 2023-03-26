eaa8859998262e11b7879ffdaaf3a7754b29e46e
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-03-26 23:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+  const lettersMap = {
+    0: "0",
+    1: "1",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz"
+  };
+
+  if (digits.length === 0) {
+    return [];
+  }
+
+  const result = [""];
+  
+  for (let i = 0; i < digits.length; i++) {
+    const currentDigit = digits[i];
+    const letters = lettersMap[currentDigit];
+    const len = result.length;
+    for (let j = 0; j < len; j++) {
+      const prevStr = result.shift();
+      for (let k = 0; k < letters.length; k++) {
+        const newStr = prevStr + letters[k];
+        result.push(newStr);
+      }
+    }
+  }
+  
+  return result.sort();
 }
 
 module.exports = letterCombinations;
